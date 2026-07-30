96616a68d6b63bc6a68db525037853d5c20c880f
Harshking001 <ekey6343@gmail.com>
2026-07-31 00:10:34 +0300

    Add videos

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,21 +1,24 @@
 import { useRef } from "react";
 import AnimateOnScroll from "./AnimateOnScroll";
 
+
 const VIDEOS = [
   {
-    src: "/assets/videos/vid.mp4",
+    src: `${import.meta.env.BASE_URL}assets/videos/vid.mp4`,
     title: "Fan Experience.",
     description: "We don't just play football we have fun too...",
   },
   {
-    src: "/assets/videos/vid3.mp4",
-    title: "Beyond Training,Building Tomorrow's Legends",
-    description: "We are back in session. Training from U7 to U17 every Saturday and Sunday from 2PM at Wendani Astro Turf. SFA home of future champions",
+    src: `${import.meta.env.BASE_URL}assets/videos/vid3.mp4`,
+    title: "Beyond Training, Building Tomorrow's Legends",
+    description:
+      "We are back in session. Training from U7 to U17 every Saturday and Sunday from 2PM at Wendani Astro Turf. SFA home of future champions",
   },
   {
-    src: "/assets/videos/vid2.mp4",
+    src: `${import.meta.env.BASE_URL}assets/videos/vid2.mp4`,
     title: "Creating Lasting Memories",
-    description: "4 categories, 4 trophies Stars game ni Moto... highlights of the concluded Rising Stars Football Tournament",
+    description:
+      "4 categories, 4 trophies. Stars game ni Moto... highlights of the concluded Rising Stars Football Tournament",
   },
 ];
 
@@ -41,7 +44,6 @@ export default function Highlights() {
       <style>{`
         .highlights-section{
           padding:5rem 1rem;
-          // background:#0d1117;
           color:#fff;
         }
 
