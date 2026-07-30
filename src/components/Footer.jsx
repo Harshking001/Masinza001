ff50f37b2d368715bd3b7e352674f604a5fe8827
Harshking001 <ekey6343@gmail.com>
2026-07-30 23:51:20 +0300

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer>
       <h3>Connect With Us</h3>
       <p>Follow our official channel for daily training sessions, match goals, and behind-the-scenes content!</p>
-      <p className="copyright">&copy; 2026 Stars Football Academy, Nairobi, Kenya. By Masinza001.</p>
+      <p className="copyright">&copy; 2026 Stars Football Academy, Nairobi, Kenya. By Harshkey.</p>
     </footer>
   )
 }
--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -3,17 +3,17 @@ import AnimateOnScroll from "./AnimateOnScroll";
 
 const VIDEOS = [
   {
-    src: "./public/assets/videos/vid.mp4",
+    src: "/assets/videos/vid.mp4",
     title: "Fan Experience.",
     description: "We don't just play football we have fun too...",
   },
   {
-    src: "./public/assets/videos/vid3.mp4",
+    src: "/assets/videos/vid3.mp4",
     title: "Beyond Training,Building Tomorrow's Legends",
     description: "We are back in session. Training from U7 to U17 every Saturday and Sunday from 2PM at Wendani Astro Turf. SFA home of future champions",
   },
   {
-    src: "./public/assets/videos/vid2.mp4",
+    src: "/assets/videos/vid2.mp4",
     title: "Creating Lasting Memories",
     description: "4 categories, 4 trophies Stars game ni Moto... highlights of the concluded Rising Stars Football Tournament",
   },
